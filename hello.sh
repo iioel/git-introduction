6dfb4b58f8913c6e95796dafb7e882795288a23a
Yoël Cornamusaz <iioel64@gmail.com>
2015-09-04 14:57:58 +0200

    Add dialog box choice

--- hello.sh
+++ hello.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-ruby hello.rb # Run rubis's script
+HELLO=$(zenity --question --text="Are you shure do you want to run hello.rb ?") 
+
+if [ $HELLO=0 ]
+then ruby hello.rb # Run rubis's script
+fi
